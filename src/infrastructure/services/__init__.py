106d8fed8e4a0e29ca65eae8fa2d318e769e3ea0
Kingprogrammer07 <gaybullayevjorabek@gmail.com>
2026-04-29 18:53:59 +0500

    feat: add FlightMaskService to the service exports

--- src/infrastructure/services/__init__.py
+++ src/infrastructure/services/__init__.py
@@ -3,11 +3,13 @@ from src.infrastructure.services.payment_card import PaymentCardService
 from src.infrastructure.services.client_transaction import ClientTransactionService
 from src.infrastructure.services.user_payment_card import UserPaymentCardService
 from src.infrastructure.services.payment_allocation import PaymentAllocationService
+from src.infrastructure.services.flight_mask import FlightMaskService
 
 __all__ = [
     'ClientService',
     'PaymentCardService',
     'ClientTransactionService',
     'UserPaymentCardService',
-    'PaymentAllocationService'
+    'PaymentAllocationService',
+    'FlightMaskService'
 ]
